de48eb6c4eb30198a7bd1567cb39dfa2f4f62134
Kyle Roddick <kyle.roddick@gmail.com>
2021-01-17 21:49:26 -0400

    add campaign 2

--- code/sentenceSentiments.R
+++ code/sentenceSentiments.R
@@ -11,12 +11,10 @@ dat <- readRDS(here('data', 'tidyData.rds'))
 
 #### Campaign 1 ####
 
-testDat <- dat %>% 
+c1Dat <- dat %>% 
   filter(campaign == '1',
          gamePlay,
-         mainCast,
-         !oneShot,
-         episode < 20) %>% 
+         !oneShot) %>% 
   mutate(splitSentences = get_sentences(text),
          sent = sentiment_by(splitSentences),
          timeMin = minute(ts) + 60 * hour(ts),
@@ -26,7 +24,7 @@ testDat <- dat %>%
   ungroup() %>% 
   mutate(indexN = row_number())
 
-testDat %>% 
+c1Dat %>% 
   ggplot(aes(x = indexN,
              y = meanSent,
              yend = 0,
@@ -36,4 +34,33 @@ testDat %>%
   geom_smooth(se = FALSE,
               colour = 'red') +
   geom_hline(yintercept = 0,
-             colour = 'white')
+             colour = 'white') +
+  labs(title = "Campaign 1")
+
+#### Campaign 2 ####
+
+c2Dat <- dat %>% 
+  filter(campaign == '2',
+         gamePlay,
+         !oneShot) %>% 
+  mutate(splitSentences = get_sentences(text),
+         sent = sentiment_by(splitSentences),
+         timeMin = minute(ts) + 60 * hour(ts),
+         lenghtSen = lengths(splitSentences)) %>% 
+  group_by(episode, timeMin) %>% 
+  summarise(meanSent = mean(sent$ave_sentiment * lenghtSen)) %>% 
+  ungroup() %>% 
+  mutate(indexN = row_number())
+
+c2Dat %>% 
+  ggplot(aes(x = indexN,
+             y = meanSent,
+             yend = 0,
+             xend = indexN,
+             group = episode)) +
+  geom_point(alpha = .2) +
+  geom_smooth(se = FALSE,
+              colour = 'red') +
+  geom_hline(yintercept = 0,
+             colour = 'white') +
+  labs(title = "Campaign 1")
