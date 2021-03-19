14a115253711aabfee3fbfa84a9c7b04da41896b
Kyle Roddick <kyle.roddick@gmail.com>
2021-03-18 22:07:58 -0300

    separate wordClouds into C1 and C2

--- code/autoUpdate.R
+++ code/autoUpdate.R
@@ -5,7 +5,7 @@ library(here)
 py_run_file(here('data', 'tojson.py'))
 source(here('code', 'wrangleData.R')) 
 1 # to select google account to read in critrolestats tables
-source(here('code', 'wordClouds.R'))
+source(here('code', 'wordCloudsC2.R'))
 source(here('code', 'sentiments.R'))
 render(here('docs', 'wordCloudsC2.Rmd'))
 render(here('docs', 'sentiments.Rmd'))
