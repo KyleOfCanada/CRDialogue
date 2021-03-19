3f9b52863f8320490ddc45003b4a82ad6a490b7e
Kyle Roddick <kyle.roddick@gmail.com>
2021-03-19 15:00:47 -0300

    refactored autoUpdate

--- code/autoUpdate.R
+++ code/autoUpdate.R
@@ -1,13 +1,11 @@
-library(reticulate)
-library(rmarkdown)
 library(here)
 
-py_run_file(here('data', 'tojson.py'))
+reticulate::py_run_file(here('data', 'tojson.py'))
 source(here('code', 'wrangleData.R')) 
 1 # to select google account to read in critrolestats tables
 runC2Guests <- FALSE # set to TRUE to run code for guests in C2
 source(here('code', 'wordCloudsC2.R'))
 source(here('code', 'sentiments.R'))
-render(here('docs', 'wordCloudsC2.Rmd'))
-render(here('docs', 'sentiments.Rmd'))
-render(here('README.Rmd'))
+rmarkdown::render(here('docs', 'wordCloudsC2.Rmd'))
+rmarkdown::render(here('docs', 'sentiments.Rmd'))
+rmarkdown::render(here('README.Rmd'))
