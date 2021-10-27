1cdbe841d6af205394999a4e3d66ca91b02f2546
Kyle Roddick <76850901+KyleOfCanada@users.noreply.github.com>
2021-10-27 13:05:23 -0300

    restyle code

--- code/autoUpdate.R
+++ code/autoUpdate.R
@@ -36,7 +36,7 @@ while (moreEpisodes) {
   ))
   if (class(fileDownloaded) == "try-error") {
     moreEpisodes <- FALSE
-  } 
+  }
 }
 
 if (episode == (episodeCount[nrow(episodeCount), 2] + 1)) {
@@ -114,13 +114,14 @@ if (episode == (episodeCount[nrow(episodeCount), 2] + 1)) {
   # run R scripts with new episodes
   source(here("code", "wrangleDataC3.R"))
   runC3Guests <- FALSE # set to TRUE to run code for guests in C3
-  source(here('code', 'wordCloudsC3.R'))
+  source(here("code", "wordCloudsC3.R"))
   # source(here('code', 'sentiments.R'))
   # source(here('code', 'bigramCloudsC2.R'))
 
   # render updated markdown docs
-  rmarkdown::render(here('code', 'RmdFiles', 'wordCloudsC3.Rmd'),
-                    output_dir = here('docs'))
+  rmarkdown::render(here("code", "RmdFiles", "wordCloudsC3.Rmd"),
+    output_dir = here("docs")
+  )
   # rmarkdown::render(here('code', 'RmdFiles', 'sentiments.Rmd'),
   #                   output_dir = here('docs'))
   # rmarkdown::render(here('code', 'RmdFiles', 'bigramsC2.Rmd'),
