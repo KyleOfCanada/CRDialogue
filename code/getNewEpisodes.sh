aab559b424f8cf1afb49b633debd6ec121f3f03d
Kyle Roddick <76850901+KyleOfCanada@users.noreply.github.com>
2023-11-06 19:18:00 -0400

    testing

--- code/getNewEpisodes.sh
+++ code/getNewEpisodes.sh
@@ -99,14 +99,10 @@ while true; do
 # Check if new episodes were downloaded
 if [ $downloaded_episodes == true ]; then
     echo "New episodes downloaded"
-    export NEW_EP="TRUE"
-    echo "$NEW_EP"  # debug
     echo "new_episode=TRUE" >> "$GITHUB_OUTPUT"
     echo "new_episode=TRUE" >> "$GITHUB_ENV"
 else
     echo "No new episodes available"
-    export NEW_EP="FALSE"
-    echo "$NEW_EP"  # debug
     echo "new_episode=FALSE" >> "$GITHUB_OUTPUT"
     echo "new_episode=FALSE" >> "$GITHUB_ENV"
 fi
