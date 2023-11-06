2359620a52a4089a48a5ee05721970d04c195d92
Kyle Roddick <76850901+KyleOfCanada@users.noreply.github.com>
2023-11-06 17:34:15 -0400

    update download function and logic

--- code/getNewEpisodes.sh
+++ code/getNewEpisodes.sh
@@ -25,58 +25,93 @@ counter=5
 
 # Function to download an episode
 download_episode() {
-    local episode_number="$1"
     local campaign_number="$1"
-    local url="https://kryogenix.org/crsearch/html/cr${campaign_number}-${episode_number}.html"
+    local episode_number="$2"
+    local url="https://www.kryogenix.org/crsearch/html/cr${campaign_number}-${episode_number}.html"
     local download_name="cr${campaign_number}-${episode_number}.html"
     
-    # Download the episode using curl
-    if curl -o "data/html/${download_name}" "$url"; then
-        echo "Downloaded $download_name"
-        return 0
+    # Use curl with -I to send a HEAD request and get the HTTP headers
+    http_headers=$(curl -I "$url" 2>&1)
+    
+    # Extract the HTTP status code from the headers
+    http_status_code=$(echo "$http_headers" | grep -i -m 1 "HTTP/1.1" | awk '{print $2}')
+    
+    # Check HTTP status and download if 200
+    if [ "$http_status_code" = "404" ]; then
+        echo "HTTP 404 Error: Episode not found: $campaign_number - $episode_number"
+        return 1
+    elif [ "$http_status_code" = "200" ]; then
+        # Download the episode using curl
+        if curl -o "data/html/${download_name}" "$url"; then
+            echo "Downloaded $download_name"
+            return 0
+        else
+            local exit_code=$?
+            echo "Failed to download $download_name (Exit Code: $exit_code)"
+            return $exit_code
+        fi
     else
-        echo "Failed to download $download_name"
+        echo "HTTP Error: Unexpected status code $http_status_code"
         return 1
     fi
 }
 
 # Main loop to download episodes
 more_episodes=true
+downloaded_episodes=false
 
-while more_episodes
+while $more_episodes
   do
-    oneshot=$((episode + .01))
+    # Check for oneshot episodes
+    oneshot=$(echo "$episode + 0.01" | bc)
     more_oneshots=true
-    while more_oneshots
+    while $more_oneshots
       do
-        download_episode "$oneshot"
-        if [ $? -ne 0 ]; then
+        download_episode "$campaign" "$oneshot"
+        if [ $? == 0 ]; then
             counter=$((counter - 1))
-            oneshot=$((oneshot + .01))
+            oneshot=$(echo "$oneshot + 0.01" | bc)
+            downloaded_episodes=true
         else
             more_oneshots=false
+        fi
+
         if [ "$counter" -lt 1 ]; then
             echo "Download limit reached!"
             break
         fi
       done
+
+    # Check for main episodes
     episode=$((episode + 1))
-    download_episode "episode"
-    if [ $? -ne 0 ]; then
+    download_episode "$campaign" "$episode"
+    if [ $? == 0 ]; then
         counter=$((counter - 1))
         episode=$((episode + 1))
+        downloaded_episodes=true
     else
         more_oneshots=false
+    fi
+    
     if [ "$counter" -lt 1 ]; then
         echo "Download limit reached!"
         break
     fi
   done
 
-# Check if there are new episodes
-if [ "$episode" -le $(tail -n 1 episodeCount.txt | cut -f 2) ]; then
+# Check if new episodes were downloaded
+if [ "$downloaded_episodes" ]; then
+    echo "New episodes downloaded"
+    export NEW_EP="TRUE"
+else
     echo "No new episodes available"
     export NEW_EP="FALSE"
-else
-    export NEW_EP="TRUE"
 fi
+
+
+#if [ "$episode" -le $(tail -n 1 episodeCount.txt | cut -f 2) ]; then
+#    echo "No new episodes available"
+#    export NEW_EP="FALSE"
+#else
+#    export NEW_EP="TRUE"
+#fi
